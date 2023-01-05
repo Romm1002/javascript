5a0cba8e35c7fc8f8c36d9f796fe79d6c6d2312b
Romm1002 <romain.chaumont@hotmail.fr>
2023-01-05 15:47:54 +0100

    style v2

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,60 +1,80 @@
-export default function Character({name, status, species, type, gender, origin, location, image}){
+export default function Character({ name, status, species, type, gender, origin, location, image }) {
     return {
         tagName: 'div',
-
+        classList: ['details'],
         children: [
             {
-              tagName: 'img',
-              attributes: {
-                src: image
-              }
+                tagName: 'img',
+                attributes: {
+                    src: image
+                }
             },
             {
-              tagName: 'div',
-              children: [
-                {
-                    tagName: 'h5',
-                    text: name,
-                },
-                {
-                    tagName: 'dl',
-                    children: [
-                        {
-                            tagName: 'dt',
-                            text: 'Status : '
-                        },
-                        {
-                            tagName: 'dd',
-                            text: status
-                        },
-                        {
-                            tagName: 'dt',
-                            text: species + ' - '
-                        },
-                        {
-                            tagName: 'dd',
-                            text: gender
-                        },
-                        {
-                            tagName: 'dt',
-                            text: 'Dernière localisation connue : '
-                        },
-                        {
-                            tagName: 'dd',
-                            text: location.name
-                        },
-                        {
-                            tagName: 'dt',
-                            text: 'Origine : '
-                        },
-                        {
-                            tagName: 'dd',
-                            text: origin.name
-                        },
-                    ]
-                },
-              ]
+                tagName: 'div',
+                classList: ['infos'],
+                children: [
+                    {
+                        tagName: 'h2',
+                        text: name,
+                    },
+                    {
+                        tagName: 'div',
+                        classList: ['row'],
+                        children: [
+                            {
+                                tagName: 'dt',
+                                text: 'Status : '
+                            },
+                            {
+                                tagName: 'dd',
+                                text: status
+                            },
+                        ]
+                    },
+                    {
+                        tagName: 'div',
+                        classList: ['row'],
+                        children: [
+                            {
+                                tagName: 'dt',
+                                text: species + ' - &nbsp;'
+                            },
+                            {
+                                tagName: 'dd',
+                                text: gender
+                            },
+                        ]
+                    },
+                    {
+                        tagName: 'div',
+                        classList: ['row'],
+                        children: [
+                            {
+                                tagName: 'dt',
+                                text: 'Dernière localisation connue : '
+                            },
+                            {
+                                tagName: 'dd',
+                                text: location.name
+                            },
+                        ]
+                    },
+                    {
+                        tagName: 'div',
+                        classList: ['row'],
+                        children: [
+                            {
+                                tagName: 'dt',
+                                text: 'Origine : '
+                            },
+                            {
+                                tagName: 'dd',
+                                text: origin.name
+                            },
+                        ]
+                    },
+                ]
             },
-          ],
+        ],
     }
 }
