f2498fc7cd76c3d569a9f94a129688675ab33989
zbrwkq <Damien.chv78@gmail.com>
2023-01-06 14:04:03 +0100

    location list

--- src/components/LocationsCardList.js
+++ src/components/LocationsCardList.js
@@ -4,6 +4,6 @@ export default function CardList(arrayOfCards) {
   return {
     tagName: 'div',
     classList: ['locations'],
-    children: arrayOfCards.map(({ id, name, type, dimension, residents }) => Card({ id: id, name: name, type: type, dimension: dimension, residents: residents, classList: ['character'] }))
+    children: arrayOfCards.map(({ id, name, type, dimension, residents }) => Card({ id: id, name: name, type: type, dimension: dimension, residents: residents, classList: ['location'] }))
   }
 }
